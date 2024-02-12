c4dbc62f2f1ac7e19555d0783ee504714abde49a
venkateshpalagiri <venkateshpalagiri@gmail.com>
2024-02-12 12:00:11 -0500

    GET, POST method

--- firstjobapp/src/main/java/com/venkatesh/firstjobapp/job/JobController.java
+++ firstjobapp/src/main/java/com/venkatesh/firstjobapp/job/JobController.java
@@ -1,6 +1,8 @@
 package com.venkatesh.firstjobapp.job;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -11,5 +13,11 @@ public class JobController {
     @GetMapping("/jobs")
     public List<Job> findAll(){
         return jobs;
+    }
+
+    @PostMapping("/jobs")
+    public String createJob(@RequestBody Job job){
+        jobs.add(job);
+        return "Job added successfully";
     }
 }
